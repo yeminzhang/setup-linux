759fd21b368a7b1a00ae3c584c06d0f3ffca81b2
yemin <yemin@mail.atzweb.com>
2017-05-16 17:47:33 +0800

    no longer lock terminal to a specific tag
    it is possible that different tags want to have different terminal
    layouts. So terminals should be able to exists in different tags.

--- X/config.h
+++ X/config.h
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "emacs", "browser", "terminal", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,11 +30,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /**
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ NULL,       "emacs",    NULL,       1,            0,           -1 },
-	{ "Gnone-terminal",  NULL,NULL,       1 << 2,       0,           -1 },
+	{ "Gnome-terminal",  NULL,NULL,       1 << 2,       0,           -1 },
 	{ NULL,       "rxvt",     NULL,       1 << 2,       0,           -1 },
+    */
 };
 
 /* layout(s) */
